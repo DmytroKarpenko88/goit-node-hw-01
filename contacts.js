c7a288000668fef49aba437b9926fe74d7c02992
Dmytro Karpenko <kardmitriy@gmail.com>
2023-06-28 21:16:35 +0300

    update

--- contacts.js
+++ contacts.js
@@ -3,25 +3,24 @@ const { nanoid } = require('nanoid');
 
 const path = require('path');
 
-//   Розкоментуй і запиши значення
 const contactsPath = path.join(__dirname, 'db/contacts.json');
 
 // TODO: задокументувати кожну функцію
 async function listContacts() {
-  // ...твій код. Повертає масив контактів.
+  // Повертає масив контактів.
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
 }
 
 async function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+  // Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
   const allContacts = await listContacts();
   const result = allContacts.find(item => item.id === String(contactId));
   return result || null;
 }
 
 async function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+  // Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
   const allContacts = await listContacts();
   const index = allContacts.findIndex(item => item.id === String(contactId));
   if (index === -1) return null;
@@ -32,7 +31,7 @@ async function removeContact(contactId) {
 }
 
 async function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту.
+  //  Повертає об'єкт доданого контакту.
   const allContacts = await listContacts();
   const newContact = {
     id: nanoid(),
